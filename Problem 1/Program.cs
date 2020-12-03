c2facda89e689734070600a9ba1ee80b4f0b3f5e
Arthur Lockman <hello@rthr.me>
2020-12-02 21:19:27 -0500

    Implementing problem 1 & 2 in C#

---
+++ Problem 1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = await System.IO.File.ReadAllLinesAsync("./input.txt") as IEnumerable<string>;
+            var numbers = input.Select(i => int.Parse(i));
+            var match1 = numbers.SelectMany(n1 => {
+                return numbers.Select(n2 => {
+                    if (n1 + n2 == 2020)
+                    {
+                        return (n1, n2);
+                    }
+                    return (-1, -1);
+                });
+            }).Where(m => m.Item1 >= 0 && m.Item2 >= 0).First();
+            Console.WriteLine("====Part 1====");
+            Console.WriteLine($"The two numbers are {match1.Item1} and {match1.Item2}. Their product is {match1.Item1 * match1.Item2}");
+
+            var match2 = numbers.SelectMany(n1 => {
+                return numbers.SelectMany(n2 => {
+                    return numbers.Select(n3 => {
+                        if (n1 + n2 + n3 == 2020)
+                        {
+                            return (n1, n2, n3);
+                        }
+                        return (-1, -1, -1);
+                    });
+                });
+            }).Where(m => m.Item1 >= 0 && m.Item2 >= 0 && m.Item3 >= 0).First();
+            Console.WriteLine("\n====Part 2====");
+            Console.WriteLine($"The three numbers are {match2.Item1}, {match2.Item2} and {match2.Item3}. Their product is {match2.Item1 * match2.Item2 * match2.Item3}");
+        }
+    }
+}
---
+++ Problem 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Problem_2
+{
+    record PasswordEntry {
+        public int Lower { get; init; }
+        public int Upper { get; init; }
+        public char Char { get; init; }
+        public string Password { get; init; }
+    }
+
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var regex = new Regex(@"(?<lower>\d+)-(?<upper>\d+) (?<char>\w+): (?<password>.*)");
+            var input = await System.IO.File.ReadAllLinesAsync("./input.txt") as IEnumerable<string>;
+            var passwordEntries = input.Select(i => {
+                var matches = regex.Match(i);
+                return new PasswordEntry {
+                    Lower = int.Parse(matches.Groups["lower"].Value),
+                    Upper = int.Parse(matches.Groups["upper"].Value),
+                    Char = matches.Groups["char"].Value.ToCharArray().First(),
+                    Password = matches.Groups["password"].Value
+                };
+            });
+            var condition1Count = passwordEntries.Where(entry => {
+                var charCount = entry.Password.Count(p => p == entry.Char);
+                return entry.Lower <= charCount && charCount <= entry.Upper;
+            }).Count();
+            Console.WriteLine($"{condition1Count} passwords satisfy condition 1.");
+
+            var condition2Count = passwordEntries.Where(entry => entry.Password[entry.Lower - 1] == entry.Char ^
+                                                                 entry.Password[entry.Upper - 1] == entry.Char).Count();
+            Console.WriteLine($"{condition2Count} passwords satisfy condition 2.");
+        }
+    }
+}
